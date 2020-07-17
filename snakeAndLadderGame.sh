499e396fee71357ccf7edc328d526d1c009c5f40
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-17 13:42:48 +0530

    [VipulA] ADD: Player gets options: NoPlay, Ladder and Snake

--- snakeAndLadderGame.sh
+++ snakeAndLadderGame.sh
@@ -2,15 +2,15 @@
 
 echo "================ Welcome to SNAKES AND LADDERS ================"
 
-#Apply values to variables
+#CONSTANTS
 SINGLE_PLAYER=1;
 POSITION_OF_PLAYER=0;
-
-playerLivePosition=0;
-
 PLAYER_GETS_NO_PLAY=0;
 PLAYER_GETS_LADDER=1;
 PLAYER_GETS_SNAKE=2;
+
+#Apply values to variables
+playerLivePosition=0;
 
 function playerThrowsDice() {
 	echo $((RANDOM%6+1))
